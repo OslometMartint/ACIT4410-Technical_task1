6a3ced94509314601608c86bd3a749b280a2f687
OslometMartinT <s331044@oslomet.no>
2019-10-02 16:09:39 +0000

    added pa11y-ci to pipeline

--- pipeline_scripts/initial_setup.sh
+++ pipeline_scripts/initial_setup.sh
@@ -1,2 +1,8 @@
+ehco "UPDATING"
 sudo apt-get update
+
+echo "PULLING REPOSITORY"
 git pull https://github.com/OslometMartint/ACIT4410-Technical_task1
+
+echo "INSTALLING pa11y-ci"
+npm install -g pa11y-ci
