ab1da2f1f7fd963443e91e0f517c3a04881712eb
OslometMartinT <s331044@oslomet.no>
2019-10-03 20:22:02 +0000

    chaned port

--- pipeline_scripts/run_docker_image.sh
+++ pipeline_scripts/run_docker_image.sh
@@ -3,17 +3,15 @@ NEW_TAG=$(git log -1 --pretty=%H)
 NEW_IMG=$NAME:$NEW_TAG
 CONTAINERS_RUNNING_PROJECT=$(docker ps -a | awk '{print $1, $2 }' | grep technical_project1/s331044 | awk '{print $1}')
 
-echo "Test echoing images"
 echo $CONTAINERS_RUNNING_PROJECT
 
 echo "Stop and remove containers start"
 for container in ${CONTAINERS_RUNNING_PROJECT}; 
 do
     `sudo docker rm -f $container`
-    done
+done
 echo "Stop and remove containers done"
 
-#Start docker container running the new image in the directory above
 echo "Attempting to run $NEW_IMG"
-`sudo docker run -d -p 3000:3000 $NEW_IMG` 
+`sudo docker run -d -p 80:80 $NEW_IMG` 
 echo "start new done"
