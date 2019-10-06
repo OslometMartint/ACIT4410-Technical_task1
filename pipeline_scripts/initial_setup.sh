74b486ca4a2727a5b9b9c67f6d3ea5d5677596f0
OslometMartinT <s331044@oslomet.no>
2019-10-06 12:11:15 +0000

    Accessibility tests

--- pipeline_scripts/initial_setup.sh
+++ pipeline_scripts/initial_setup.sh
@@ -10,5 +10,12 @@ sudo apt install nodejs
 echo "INSTALLING eslint"
 sudo npm i -g eslint
 
+echo "INSTALLING htmlhint"
+sudo npm install htmlhint -g
+
+#chromium-browser is necessary to run pa11y-ci
+echo "INSTALLING chromium-browser"
+sudo apt install -y chromium-browser
+
 echo "INSTALLING pa11y-ci"
 npm i pa11y-ci
